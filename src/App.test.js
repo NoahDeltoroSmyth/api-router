2673bbf02ce001b9753828852ae35373d8259dab
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-01 17:41:52 -0800

    component based tests written in HomeView.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('app renders loading message', () => {
   render(<App />);
   const linkElement = screen.getByText(/please stand by while we fetch your space data.../i);
   expect(linkElement).toBeInTheDocument();
---
+++ src/views/HomeView/HomeView.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import HomeView from './HomeView';
+
+test('HomeView renders a list of characters and their details', async () => {
+  render(
+    <MemoryRouter>
+      <HomeView />
+      );
+    </MemoryRouter>
+  );
+
+  const loading = screen.getByRole('heading', {
+    name: /please stand by while we fetch your space data.../i,
+  });
+  expect(loading).toBeInTheDocument();
+
+  const image = await screen.findByRole('img', { name: /mooncake/i }, { timeout: 4000 });
+  expect(image).toBeInTheDocument();
+
+  const altText = await screen.findByAltText(/mooncake/i);
+  expect(altText).toBeInTheDocument();
+
+  const header = screen.getByRole('heading', { name: /mooncake/i });
+  expect(header).toBeInTheDocument();
+
+  //   const allHeaders = await screen.findAllByRole('heading', { level: 1 });
+  //   expect(allHeaders).toEqual(47);
+});
