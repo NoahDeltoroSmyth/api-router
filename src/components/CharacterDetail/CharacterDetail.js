93262e45a7e43d829c5dc2f9d0ee0f8d90aabdb4
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-03 15:31:17 -0800

    refactored character prop in detailview and characterdetail to try and fix useparams, but hasn't worked yet

--- src/components/CharacterDetail/CharacterDetail.js
+++ src/components/CharacterDetail/CharacterDetail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import './CharacterDetail.css';
 
-const CharacterDetail = ({ name, species, origin, img_url, status }) => {
+const CharacterDetail = ({ character }) => {
   const history = useHistory();
   function handleButtonClick() {
     history.push('/');
@@ -10,11 +10,11 @@ const CharacterDetail = ({ name, species, origin, img_url, status }) => {
 
   return (
     <div>
-      <h1 className="name-header">{name}</h1>
-      <img className="detail-img" src={img_url} alt={name} />
-      <p>species: {species}</p>
-      <p>origin: {origin}</p>
-      <p>Dead or Alive?: {status}</p>
+      <h1 className="name-header">{character.name}</h1>
+      <img className="detail-img" src={character.img_url} alt={character.name} />
+      <p>species: {character.species}</p>
+      <p>origin: {character.origin}</p>
+      <p>Dead or Alive?: {character.status}</p>
       <button onClick={handleButtonClick}>Go back to character list</button>
     </div>
   );
