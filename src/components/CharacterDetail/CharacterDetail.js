348dc4a04ca5b7a7e608b717b3f432ce49c376b7
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-03 17:12:51 -0800

    cleaned up console logs

--- src/components/CharacterDetail/CharacterDetail.js
+++ src/components/CharacterDetail/CharacterDetail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import './CharacterDetail.css';
 
-const CharacterDetail = ({ character, id }) => {
+const CharacterDetail = ({ character }) => {
   const history = useHistory();
   function handleButtonClick() {
     history.push('/');
--- src/services/apiRoute.js
+++ src/services/apiRoute.js
@@ -1,13 +1,11 @@
 export const fetchCharacters = async () => {
   const resp = await fetch(`https://finalspaceapi.com/api/v0/character`);
   const jsonData = await resp.json();
-  console.log('jsonData', jsonData);
   return jsonData;
 };
 
 export const fetchCharactersById = async (id) => {
   const resp = await fetch(`https://finalspaceapi.com/api/v0/character/${id}`);
   const characterId = await resp.json();
-  console.log('characterId', characterId);
   return characterId;
 };
